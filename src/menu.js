15d30d298d4535d4f43024f49c401d5936ca541b
Wlad <wladkrychin@gmail.com>
2021-06-10 11:36:53 +0200

    1

--- src/menu.js
+++ src/menu.js
@@ -5,9 +5,36 @@ const Theme = {
 
   const checkBox = document.querySelector('#theme-switch-toggle')
   checkBox.addEventListener('change', onChangeInput)
-  const body = document.querySelector('body')
-  function onChangeInput (){
-      body.classList.toggle(Theme.DARK)
-      
-
+   
+  function onChangeInput(event) {
+    event.currentTarget.checked ? onCheckedInput() : offCheckedInput();
   }
+  
+  function onCheckedInput() {
+    document.body.classList.add(Theme.DARK);
+    document.body.classList.remove(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.DARK);
+    checkBox.checked = true;
+  }
+  
+  function offCheckedInput() {
+    document.body.classList.add(Theme.LIGHT);
+    document.body.classList.remove(Theme.DARK);
+    localStorage.setItem('theme', Theme.LIGHT);
+    checkBox.checked = false;
+  }
+  
+  function currentThemeWebSite() {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === Theme.LIGHT || savedTheme === null) {
+      offCheckedInput();
+      return;
+    }
+  
+    if (savedTheme === Theme.DARK) {
+      onCheckedInput();
+      return;
+    }
+  }
+  
+  currentThemeWebSite();
